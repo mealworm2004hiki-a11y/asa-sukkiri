664929214807277a48ecdd2d4a48148c1ed443e0
mealworm2004hiki-a11y <mealworm2004hiki@gmail.com>
2026-07-19 08:43:03 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='asa-sukkiri-v5';
+const CACHE='asa-sukkiri-v6';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-maskable.svg'];
 self.addEventListener('install',e=>{
   self.skipWaiting();
